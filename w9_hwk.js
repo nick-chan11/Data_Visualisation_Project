311023cffb16e3668074c6976114feedd2603714
Nicholas Chan <ncha0085@student.monash.edu>
2025-10-08 13:41:39 +1100

    Added bubble chart to js script

--- w9_hwk.js
+++ w9_hwk.js
@@ -3,3 +3,9 @@ var vg_1 = "https://raw.githubusercontent.com/ncha0085/FIT3179/refs/heads/main/f
 vegaEmbed("#flight_map", vg_1).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+
+var vg_2 = "https://raw.githubusercontent.com/ncha0085/FIT3179/refs/heads/main/arrivals_departures_bubble_chart.vg.json";
+
+vegaEmbed("#bubble_chart", vg_2).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
