1f20769b5e9c0f7f35226dfc07b303fb33d3e5fa
Nicholas Chan <ncha0085@student.monash.edu>
2025-10-09 18:21:31 +1100

    refactored vg.json files. redirected file paths on w10_hwk.js to reflect factored vg.json files

--- js/w10_hwk.js
+++ js/w10_hwk.js
@@ -1,10 +1,10 @@
-var vg_1 = "https://raw.githubusercontent.com/ncha0085/FIT3179/refs/heads/main/flight_path_map.vg.json";
+var vg_1 = "https://raw.githubusercontent.com/ncha0085/FIT3179/refs/heads/main/js/flight_path_map.vg.json";
 
 vegaEmbed("#flight_map", vg_1).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-var vg_2 = "https://raw.githubusercontent.com/ncha0085/FIT3179/refs/heads/main/arrivals_departures_bubble_chart.vg.json";
+var vg_2 = "https://raw.githubusercontent.com/ncha0085/FIT3179/refs/heads/main/js/arrivals_departures_bubble_chart.vg.json";
 
 vegaEmbed("#bubble_chart", vg_2).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
